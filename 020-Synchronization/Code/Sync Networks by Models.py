9c831fe58f49482c9bccb13a266df774105695c2
Alireza-A-Baneh <alireza.a.baneh@gmail.com>
2025-02-23 10:21:00 +0330

    Set Initial data as json and read data in the code!

--- 020-Synchronization/Code/Sync Networks by Models.py
+++ 020-Synchronization/Code/Sync Networks by Models.py
@@ -11,7 +11,7 @@ import random
 
 global START_TIME, TIME_ID
 global CODE_DIR, NET_DIR, DEST_DIR
-global DICT_Run
+global DICT_RUN
 global DICT_MODEL, DICT_NETWORKSglobal, list_result_xlsx_path, list_dict_phi
 global DF_GRAPH, NUM_NODES
 global NUM_MODELS, NAMES_MODELS
@@ -70,7 +70,23 @@ def f_net_newfolder():
     shutil.copytree(PARENT_DIR + "\\Code" , DEST_DIR+"\\Initial Parameters", dirs_exist_ok=True)
 
 
+def f_read_parameters():
+    global DICT_NETWORKS, DICT_RUN, DICT_MODEL
+    global NUM_MODELS, LIST_MODELS, NUM_TOTAL_STEPS
 
+    with open("Initial Sync Parameters.json", "r") as json_file:
+        DATA = json.load(json_file)
+        
+    #Seperate data from json file in different dictionaries to (Easy to Access)
+    DICT_NETWORKS   = DATA["NETWORKS"]
+    DICT_RUN        = DATA["Run"]
+    DICT_MODEL      = DATA["MODELS"]
+    
+    NUM_MODELS = DICT_MODEL["FIXED_PAR"]["NUM_MODELS"]
+    LIST_MODELS = DICT_MODEL["FIXED_PAR"]["L_MODELS"]
+    NUM_TOTAL_STEPS = DICT_RUN["START_SYNC_NUM_STEPS"] + DICT_RUN["De_SYNC_NUM_STEPS"] + DICT_RUN["END_SYNC_NUM_STEPS"]
+
+    
 
 
 
