eff38bd44bf71b3139b15f66836f94de5e671ae6
Alireza-A-Baneh <alireza.a.baneh@gmail.com>
2025-02-23 11:36:50 +0330

    CodeModification-Functions for initializing the Kuramoto model

--- 020-Synchronization/Code/Sync Networks by Models.py
+++ 020-Synchronization/Code/Sync Networks by Models.py
@@ -20,7 +20,7 @@ global arr_models_op_coherence, arr_models_op_phase
 global index_sheet
 
 
-def f_net_newfolder():
+def f_sync_newfolder():
     global TIME_ID, START_TIME
     global CODE_DIR, NET_DIR, DEST_DIR, RUN_NUMBER
     
@@ -122,8 +122,144 @@ def f_read_graph(g_name):
      
  
     
+    
+def arr_type(the_type, model_name, case, address_net):
+    
+    if(the_type != "LIKE_OTHER_MODEL"):
+        arr_value = np.zeros(NUM_NODES)
+        
+        if(the_type     == "NORMAL_RANDOM"):
+            key_mean    = "MEAN" + case
+            key_std     = "STD" + case
+            random_std  = DICT_MODEL[model_name][key_std]
+            random_mean = DICT_MODEL[model_name][key_mean]
+            arr_value   = np.random.normal(random_mean, random_std, NUM_NODES)
+            
+        elif(the_type   == "UNIF_RANDOM"):
+            key_max     = "MAX" + case
+            key_min     = "MIN" + case
+            random_max  = DICT_MODEL[model_name][key_max]
+            random_min  = DICT_MODEL[model_name][key_min]
+            arr_value   = np.random.uniform(low = random_min, high = random_max, size = NUM_NODES)
+        
+        elif(the_type   == "ALL_SAME_RANDOM"):
+            key_max     = "MAX" + case
+            key_min     = "MIN" + case
+            random_max  = DICT_MODEL[model_name][key_max]
+            random_min  = DICT_MODEL[model_name][key_min]
+            arr_value   = arr_value + np.random.uniform(low = random_min, high = random_max)
+            
+        elif(the_type   == "ALL_SAME_FIXED"):
+            key_value   = "VALUE" + case
+            fixed_value = DICT_MODEL[model_name][key_value]
+            arr_value   = arr_value + fixed_value
+            
+        elif(the_type   == "GIVEN_LIST"):
+            key_value   = "VALUE" + case
+            fixed_value = DICT_MODEL[model_name][key_value]
+            arr_value   = arr_value + fixed_value
+            
+        elif(the_type   == "GIVEN_COLUMN"):
+            key_scale       = "SCALE" + case
+            key_scale_value = "VALUE_SCALE" + case
+            key_sheet       = "SHEET_XLSX" + case
+            key_column      = "COLUMN" + case
+            
+            sheet_xlsx  = DICT_MODEL[model_name][key_sheet]
+            column_xlsx = DICT_MODEL[model_name][key_column]
+            scale       = DICT_MODEL[model_name][key_scale]
+            
+            if(sheet_xlsx == "NODES"):
+                arr_value = pd.read_excel(NET_DIR + address_net, sheet_name = index_sheet-1)[column_xlsx]
+                if (scale == "MAX"):
+                    value_scale   = DICT_MODEL[model_name][key_scale_value]
+                    arr_value = np.array(arr_value) / np.max(arr_value) * value_scale
+                elif (scale == "MEAN"):
+                    value_scale   = DICT_MODEL[model_name][key_scale_value]
+                    arr_value = np.array(arr_value) / np.mean(arr_value) * value_scale
+                else:
+                    arr_value = np.array(arr_value)
+
+                
+    elif(the_type == "LIKE_OTHER_MODEL"):
+        key_like  = "LIKE_MODEL" + case
+        like_model_index = DICT_MODEL[model_name][key_like]
+        if (case == "_K"):
+            arr_value = arr_models_k[like_model_index,:]
+        if (case == "_G"):
+            arr_value = arr_models_g[like_model_index,:]
+        elif (case == "_OMEGA"):
+            arr_value = arr_models_omega[like_model_index,:]
+        elif (case == "_PHI"):
+            arr_value = arr_models_phi[like_model_index,:]
+        elif (case == "_AMPLI"):
+            arr_value = arr_models_ampli[like_model_index,:]
+        elif (case == "_NOISE"):
+            arr_value = arr_models_noise[like_model_index,:]
+            
+    return arr_value
+    
 
 
+def f_k_models_op_sync(model, step):
+    global arr_models_op_coherence, arr_models_op_phase
+    
+    parameter_real = sum(np.cos(arr_models_phi[model,:])) / NUM_NODES
+    parameter_img = sum(np.sin(arr_models_phi[model,:])) / NUM_NODES
+
+    arr_models_op_phase[model, step] = np.arctan2(parameter_img, parameter_real) % (2 * np.pi)
+    arr_models_op_coherence[model, step] = np.sqrt(parameter_real**2 + parameter_img**2)
+
+
+
+def f_initialize_models(address_net):
+    #OK
+    global list_dict_phi, list_dict_noise
+    global arr_models_k, arr_models_g, arr_models_omega, arr_models_phi
+    global arr_models_ampli, arr_models_noise, temp_arr_phi
+    global arr_models_op_phase, arr_models_op_coherence
+
+    list_dict_phi           = []
+    list_dict_noise         = []
+    arr_models_k            = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_g            = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_omega        = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_phi          = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_ampli        = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_noise        = np.zeros((NUM_MODELS, NUM_NODES))
+    temp_arr_phi            = np.zeros((NUM_MODELS, NUM_NODES))
+    arr_models_op_phase     = np.zeros((NUM_MODELS, NUM_TOTAL_STEPS))
+    arr_models_op_coherence = np.zeros((NUM_MODELS, NUM_TOTAL_STEPS))
+
+    for m in range(NUM_MODELS):
+        
+        model_name = LIST_MODELS[m]
+        
+        # To use the type of parameters
+        k_type      = DICT_MODEL[model_name]["TYPE_K"]
+        g_type      = DICT_MODEL[model_name]["TYPE_G"]
+        omega_type  = DICT_MODEL[model_name]["TYPE_OMEGA"]
+        phi_type    = DICT_MODEL[model_name]["TYPE_PHI"]
+        #ampli_type  = DICT_MODEL[model_name]["TYPE_AMPLI"]
+        noise_type  = DICT_MODEL[model_name]["TYPE_NOISE"]
+        
+        # To set initial values based on .jason file
+        arr_models_k[m,:]       = arr_type(k_type, model_name, "_K", address_net)
+        arr_models_g[m,:]       = arr_type(g_type, model_name, "_G", address_net)
+        arr_models_omega[m,:]   = arr_type(omega_type, model_name, "_OMEGA", address_net)
+        arr_models_phi[m,:]     = arr_type(phi_type, model_name, "_PHI", address_net)
+        #arr_models_ampli[m,:]   = arr_type(ampli_type, model_name, "_AMPLI", address_net)
+        arr_models_noise[m,:]   = arr_type(noise_type, model_name, "_NOISE", address_net)
+        
+        # to create a dict for saving as xlsx
+        list_dict_phi.append(dict())
+        list_dict_noise.append(dict())
+
+        #To save the phi of staep 0 (Noise should be saved from step 1!)
+        list_dict_phi[m]["0"]   = arr_models_phi[m,:].copy()
+        
+        # To calculate the ort\der parameter of step 0!
+        f_k_models_op_sync(m, 0)
 
 
 
@@ -150,12 +286,13 @@ def f_read_graph(g_name):
 
 
 def f_set_models_on_graphs():
-    
+
     global index_sheet
+
     f_address_input_networks()
     
-    for rep in range(DICT_Run["TOTAL_REPEAT"]):
-        print(f"\n\n\tRepeat: {rep+1} of {DICT_Run['TOTAL_REPEAT']}")
+    for rep in range(DICT_RUN["TOTAL_REPEAT"]):
+        print(f"\n\n\tRepeat: {rep+1} of {DICT_RUN['TOTAL_REPEAT']}")
         print(f"\n\t\tRunning {NUM_MODELS} Model(s) at the same time on:")
         network_counter = 1
         # loop on all the networks that we want to put the models on them.
